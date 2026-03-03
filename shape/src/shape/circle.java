fbd2ab107705d93fc831e79b2f1fca183e80c37b
MUKUND <mukundkt.thakur@gmail.com>
2026-03-04 00:18:10 +0530

    Add files via upload

---
+++ shape/src/shape/circle.java
@@ -0,0 +1,7 @@
+package shape;
+public class circle {
+	public double area (double radius) {
+		return 3.14 * radius * radius;
+			}
+	}
+
---
+++ shape/src/shape/triangle.java
@@ -0,0 +1,6 @@
+package shape;
+public class triangle {
+	public double area(double base, double height) {
+		return 0.5 * base * height;
+	}
+}
